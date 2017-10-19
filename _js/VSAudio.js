b24a6812bf06c63d9b524c7fc3e6885dce5490eb
Madison Aster <info@MadisonAster.com>
2017-10-18 23:13:23 -0700

    abstracted VSVideo and VSAudio

---
+++ _js/VSAudio.js
@@ -0,0 +1,23 @@
+/*
+aSources = [
+    './_Assets/VSAudio/tiger.mp3',
+    './_Assets/VSAudio/girl.mp3',
+    './_Assets/VSAudio/vader.mp3',
+];
+        
+aSourceDurations = {
+    './_Assets/VSAudio/girl.mp3':184710,
+    './_Assets/VSAudio/tiger.mp3':144090,
+    './_Assets/VSAudio/vader.mp3':255290,
+};
+*/
+aSources = [
+'./_Assets/VSAudio/Butthole Surfers - Pepper.mp3',
+ './_Assets/VSAudio/Deadmau5 - Strobe.mp3',
+ './_Assets/VSAudio/Lemaitre - Nishio.mp3'
+];
+aSourceDurations = {
+'./_Assets/VSAudio/Butthole Surfers - Pepper.mp3': 297534.688,
+ './_Assets/VSAudio/Deadmau5 - Strobe.mp3': 637100.408,
+ './_Assets/VSAudio/Lemaitre - Nishio.mp3': 212062.0
+};
